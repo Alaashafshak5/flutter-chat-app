c272c18ef464edf0924a00ae92227465a1923c60
alaashafshak5 <alaa@dynamixagency.com>
2022-06-22 15:46:14 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+app.get('/', (req, res) => {
+   res.send("Node Server is running. Yay!!")
+})
+
+const socketio = require('socket.io')(http)
+
+socketio.on("connection", (userSocket) => {
+    userSocket.on("send_message", (data) => {
+        userSocket.broadcast.emit("receive_message", data)
+    })
+})
+
+http.listen(process.env.PORT)
